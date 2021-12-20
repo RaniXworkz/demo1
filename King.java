072a44328da217c90b679f2a1838c0a6fd274706
RaniXorkz <raniattigeri.xworkz@gmail.com>
2021-12-20 17:59:31 +0530

    king file is added

---
+++ King.java
@@ -0,0 +1,18 @@
+class King
+{
+public static void main(String[]b)
+{
+short []ids={11,10,13,42,45,34,89,67,41,82};
+System.out.println(ids[0]);
+System.out.println(ids[1]);
+System.out.println(ids[2]);
+System.out.println(ids[3]);
+System.out.println(ids[4]);
+System.out.println(ids[5]);
+System.out.println(ids[6]);
+System.out.println(ids[7]);
+System.out.println(ids[8]);
+System.out.println(ids[9]);
+}
+}
+
